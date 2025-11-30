a2f8e5def15588c7291867441605d166136db716
Lpawlu10 <lydia.pawlus@gmail.com>
2025-11-30 21:42:50 +1100

    feat: update privacy and manage pages with improved structure and styling

--- src/app/pages/privacy/privacy.page.ts
+++ src/app/pages/privacy/privacy.page.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonLabel, IonItem} from '@ionic/angular/standalone';
+import { IonContent, 
+         IonHeader, 
+         IonTitle, 
+         IonToolbar, 
+         IonCard, 
+         IonCardContent, 
+         IonCardHeader, 
+         IonCardSubtitle, 
+         IonCardTitle, 
+         IonList, 
+         IonLabel, 
+         IonItem,
+        } from '@ionic/angular/standalone';
+        
 
-@Component({
+
+  @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonLabel, IonItem]
+  imports: [IonContent, 
+            IonHeader, 
+            IonTitle, 
+            IonToolbar, 
+            CommonModule, 
+            FormsModule, 
+            IonCard, 
+            IonCardContent, 
+            IonCardHeader, 
+            IonCardSubtitle, 
+            IonCardTitle, 
+            IonList, 
+            IonLabel, 
+            IonItem]
 })
 export class PrivacyPage implements OnInit {
 
