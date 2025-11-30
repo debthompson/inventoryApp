1f170b74f1c0a47722e1413b37e3fed811bc5147
Lpawlu10 <lydia.pawlus@gmail.com>
2025-11-30 18:13:14 +1100

    feat: enhance privacy page layout and content

--- src/app/pages/privacy/privacy.page.ts
+++ src/app/pages/privacy/privacy.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonLabel, IonItem} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonLabel, IonItem]
 })
 export class PrivacyPage implements OnInit {
 
@@ -16,5 +16,4 @@ export class PrivacyPage implements OnInit {
 
   ngOnInit() {
   }
-
 }
