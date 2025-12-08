7474fd587106da64542d63fa8f93ac93b73e47dd
debthompson <debthompson@users.noreply.github.com>
2025-12-08 15:52:20 +1100

    Updates after adding a new page.

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,9 +1,9 @@
 /* 
  * PROG 2005 - Programming Mobile Systems
  * Assessment 3
- * Group: Debralee Thompson, 
+ * Group: Debralee Thompson
  * File: src/app/tabs/tabs.page.ts
- * Purpose: Defines the tab layout component and loads Ionic tabs, icons, and routing support.
+ * Purpose: Defines the tab layout component and loads Ionic tabs, icons, and routing support
  */
 
 // Imports
@@ -11,31 +11,30 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import * as allIcons from 'ionicons/icons';
-//import { RouterLink } from '@angular/router';
 
 @Component({
-  // The component selector used in HTML
-  selector: 'app-tabs',
+	// The component selector used in HTML
+	selector: 'app-tabs',
 
-  // The HTML template for the tabs layout
-  templateUrl: 'tabs.page.html',
+	// The HTML template for the tabs layout
+	templateUrl: 'tabs.page.html',
 
-  // Styling for this component
-  styleUrls: ['tabs.page.scss'],
+	// Styling for this component
+	styleUrls: ['tabs.page.scss'],
 
-  // Mark this as a standalone component (no module required)
-  standalone: true,
+	// Mark this as a standalone component (no module required)
+	standalone: true,
 
-  // Ionic components and RouterLink available to this component
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel /*RouterLink*/],
+	// Ionic components and RouterLink available to this component
+	imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 
 export class TabsPage {
-  // Needed so child components can access Angular services
-  public environmentInjector = inject(EnvironmentInjector);
+	// Needed so child components can access Angular services
+	public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
-    // Makes all Ionicons available for use in the templates
-    addIcons(allIcons);
-  }
+	constructor() {
+		// Makes all Ionicons available for use in the templates
+		addIcons(allIcons);
+	}
 }
